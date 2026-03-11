f505e40aad1d7e4c016acf8edf5f5b8122897844
Rohit Guleria <rohitmatrix2004@gmail.com>
2026-03-12 05:12:36 +0530

    added stripping operating line

--- Matlab/distillation_simulation.m
+++ Matlab/distillation_simulation.m
@@ -14,7 +14,7 @@ figure
 plot(x,y,'b','LineWidth',2)
 hold on
 
-plot(x,x,'k--','LineWidth',1.5)
+plot(x,x,'Color',[0 0.6 0.6],'LineStyle','--','LineWidth',1.8)
 
 xlabel('Liquid mole fraction (x)')
 ylabel('Vapor mole fraction (y)')
@@ -22,6 +22,7 @@ title('VLE Equilibrium Curve for Ethanol-Water System')
 
 legend('Equilibrium curve','y = x')
 grid on
+
 
 % Distillation specifications
 xD = 0.95;   
@@ -35,9 +36,9 @@ plot(x,y_rect,'r','LineWidth',2)
 
 legend('Equilibrium curve','y = x','Rectifying operating line')
 
+
 xF=0.4;    
 q=1;      
-
 
 if q==1
     x_q=[xF xF];
@@ -49,3 +50,24 @@ else
 end
 
 legend('Equilibrium curve','y = x','Rectifying line','Feed line (q-line)')
+
+
+m = R/(R+1);
+c = xD/(R+1);
+
+if q==1
+    x_int = xF;
+    y_int = m*x_int + c;
+else
+    m_q = q/(q-1);
+    c_q = -xF/(q-1);
+    
+    x_int = (c_q - c)/(m - m_q);
+    y_int = m*x_int + c;
+end
+
+y_strip = ((y_int - xB)/(x_int - xB))*(x - xB) + xB;
+
+plot(x,y_strip,'m','LineWidth',2)
+
+legend('Equilibrium curve','y = x','Rectifying line','Feed line','Stripping line')
